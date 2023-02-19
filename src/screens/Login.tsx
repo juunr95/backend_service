8daea53bcbf3d54a3bb22b462601f4eab7f90f3a
Claudio Cesar <junior08121995@gmail.com>
2023-02-19 14:57:39 -0300

    Change icon, error message on login screen

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,6 +1,7 @@
 import { useSignInEmailPassword } from '@nhost/react'
 import { FormEvent } from "react"
 import { useNavigate } from 'react-router-dom';
+import { Spinner } from '../components/Spinner';
 
 export const Login = () => {
   const { signInEmailPassword, isLoading, isSuccess, needsEmailVerification, isError, error } =
@@ -18,19 +19,20 @@ export const Login = () => {
     }
   }
 
+  if (isLoading) {
+    return <Spinner/>
+  }
+
   return (
     <section className="bg-gray-50 dark:bg-gray-900">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-        <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
-          <img className="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo" />
-          Flowbite
-        </a>
         <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
               Sign in to your account
             </h1>
             <form onSubmit={handleSubmit} className="space-y-4 md:space-y-6">
+            { error ? <span className="text-center text-red-500">{error.message}</span> : <></>}
               <div>
                 <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
                 <input type="email" autoComplete="off" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" />
